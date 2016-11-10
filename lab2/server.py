3c8f19ec30ad4d621300ff5d1a424902bd4ca591
Oisin Cleary <oisin@Oisins-MacBook-Pro-2.local>
2016-11-10 22:24:30 +0000

    set up basic server

--- lab2/server.py
+++ lab2/server.py
@@ -1,5 +1,8 @@
 import socket
 import sys
+import threading
+import time
+import errno
 from multiprocessing import Pool, TimeoutError
 
 class ThreadedServer(object):
@@ -9,29 +12,73 @@ class ThreadedServer(object):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
-        pool = Pool(processes=4) 
+        self.pool = Pool(processes=4) 
 
     def listen(self):
         self.sock.listen(5)
         while True:
             client, address = self.sock.accept()
-            client.settimeout(60)
-            pool.apply_async(self.listenToClient, (client,address))
+            client.settimeout(15)
+            # print "starting thread"
+            # res = self.pool.apply_async(self.listenToClient, (client,address))
+            # print res.get(timeout = 10)
+            threading.Thread(target = self.listenToClient,args = (client,address)).start()
 
     def listenToClient(self, client, address):
-        size = 1024
+        print "thread started"
         while True:
-            try:
-                data = client.recv(size)
-                if data:
-                    # Set the response to echo back the recieved data 
-                    response = data
-                    client.send(response)
-                else:
-                    raise error('Client disconnected')
-            except:
+            input = self.recvWithTimeout(client,10)
+            if input.startswith("HELO"):
+                client.sendall(input + "\nIP:37.228.254.66\nPort:8000\nStudentID:13325102\n")
+            elif input == "KILL_SERVICE\n":
+                print "thread ended"
                 client.close()
                 return False
+            elif input == "":
+                print "no data recived thread ended"
+                client.close()
+                return False
+            else:
+                time.sleep(1)
+        
+        
+
+    def recvWithTimeout(self, client, timeout):
+        totalData = []
+        client.setblocking(False)
+        begin = time.time()
+        while True:
+            #if you got some data, then break after timeout
+            if totalData and time.time()-begin > timeout:
+                break
+         
+            #if you got no data at all, wait a little longer, twice the timeout
+            elif time.time()-begin > timeout*2:
+                break
+
+            try:
+                data = client.recv(10)
+                if data:
+                    totalData.append(data)
+                    begin = time.time()
+                else:
+                    time.sleep(0.1)
+            except socket.error, e:
+                err = e.args[0]
+                if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+                    time.sleep(1)
+                    continue
+                else:
+                    # a "real" error occurred
+                    print e
+                    sys.exit(1)
+            except:
+                print sys.exc_info()[0]
+                print "closing connection"
+                client.close()
+                return ""
+        finalData =  "".join(totalData)
+        return finalData
 
 if __name__ == "__main__":
     # port_num = sys.argv[1]
