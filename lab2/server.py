7c8bfeb7d1616a788e27669ddaf82d8671ac5e42
Oisin Cleary <oisin@Oisins-MacBook-Pro-2.local>
2016-11-16 18:31:45 +0000

    changed ip

--- lab2/server.py
+++ lab2/server.py
@@ -10,7 +10,7 @@ class ThreadedServer(object):
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.ip = "37.228.254.66"
+        self.ip = "10.62.0.234"
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
