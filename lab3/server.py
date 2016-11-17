162d164a9034a7d3da8f2d8c723611485876e5b9
Oisin Cleary <oisin@Oisins-MacBook-Pro-2.local>
2016-11-17 12:24:37 +0000

    Initial server from lab 2

---
+++ lab3/server.py
@@ -0,0 +1,94 @@
+import socket
+import sys
+import threading
+import time
+import errno
+import Queue
+
+
+class ThreadedServer(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.ip = "10.62.0.234"
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+        self.numWorkers = 3
+        self.q = Queue.Queue(maxsize=1)
+
+    def listen(self):
+        for i in range(self.numWorkers):
+            thread = threading.Thread(target=self.listenToClient)
+            thread.setDaemon(True)
+            thread.start()
+        self.sock.listen(5)
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(15)
+            self.q.put((client, address), True)
+
+    def listenToClient(self):
+        while True:
+            client, address = self.q.get()
+            while True:
+                input = self.recvWithTimeout(client, 10)
+                if input.startswith("HELO"):
+                    input = input[:-1]
+                    client.sendall(input + "\nIP:"+self.ip+"\nPort:"+str(self.port)+"\nStudentID:13325102\n")
+                elif input == "KILL_SERVICE\n":
+                    print "kill service recieved job ended"
+                    client.close()
+                    self.q.task_done()
+                    break
+                elif input == input.startswith("JOIN_CHATROOM:"):
+                    message = split(input, "\n")
+                    print message[0]
+                elif input == "":
+                    print "no data recived job ended"
+                    client.close()
+                    self.q.task_done()
+                    break
+                else:
+                    time.sleep(1)
+
+    def recvWithTimeout(self, client, timeout):
+        totalData = []
+        client.setblocking(False)
+        begin = time.time()
+        while True:
+            # if you got some data, then break after timeout
+            if totalData and time.time()-begin > timeout:
+                break
+            # if you got no data at all, wait a little longer, twice the timeout
+            elif time.time()-begin > timeout*2:
+                break
+
+            try:
+                data = client.recv(10)
+                if data:
+                    totalData.append(data)
+                    begin = time.time()
+                else:
+                    time.sleep(0.1)
+            except socket.error, e:
+                err = e.args[0]
+                if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+                    time.sleep(1)
+                    continue
+                else:
+                    print sys.exc_info()[0]
+                    print "closing connection"
+                    client.close()
+                    return ""
+            except:
+                print sys.exc_info()[0]
+                print "closing connection"
+                client.close()
+                return ""
+        finalData = "".join(totalData)
+        return finalData
+
+if __name__ == "__main__":
+    port_num = int(sys.argv[1])
+    ThreadedServer('0.0.0.0', port_num).listen()
